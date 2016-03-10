955d11c1be1b9771a4ebdf85b0a3667051741f4e
Petra Zabza Vidnerová <petra.vidnerova@gmail.com>
2016-03-10 21:26:29 +0100

    solved 33

--- 33.py
+++ 33.py
@@ -1,27 +1,60 @@
 def foolDivide(x,y):
-    del = [] 
-    for x in str(x):
-        for y in str(y):
-            if x == y:
-                del.append(x)
-    for digit in del:
-        x = int( str(x).replace(digit,"") )
-        y = int( str(y).replace(digit,"") )
-    return (x,y)
+    todel = [] 
+    for xs in str(x):
+        for ys in str(y):
+            if xs == ys:
+                todel.append(xs)
+    for digit in todel:
+        xs = str(x).replace(digit,"") 
+        ys =  str(y).replace(digit,"")
+        if xs == "":
+            x = 1
+        else:
+            x = int(xs)
+        if ys == "":
+            y = 1
+        else:
+            y = int(ys)
+    if y == 0:
+        return (x,y,False)
+    return (x,y,bool(todel))
+
+def divide(x,y):
+    for i in range(x+1,1,-1):
+        if x % i == 0 and y % i == 0:
+            x = x // i
+            y = y // i
+    return (x,y) 
 
 def isCurious(x,y):
-    xs, ys = x, y 
-    for i in range(99):
-        if xs % i == 0 and ys % i == 0:
-            xs = xs // i
-            ys = ys // i
-    if (xs,ys) == foolDivide(x,y):
-        return True
-    else:
+    if x == y:
+        return False
+    if x / y >= 1:
+        return False
+        
+    if x % 10 == 0 and y % 10 == 0:
         return False
 
+    (x1, y1) = divide(x,y)
+    (x2, y2, divided) = foolDivide(x,y)
+    if not divided:
+        return False
+    else:
+        return x1/y1 == x2/y2 
 
+curious_tuples = []         
 for x in range(10,99):
     for y in range(10,99):
         if isCurious(x,y):
-            print(x,y) 
+            curious_tuples.append((x,y))
+
+
+print(curious_tuples)
+
+x, y = 1, 1
+for (a, b) in curious_tuples:
+    x *= a
+    y *= b
+
+print(x, y)
+print(divide(x,y))
